70121da4b18699dee46cd90afcc57ea6f952763c
dbhabad <109644648+dbhabad@users.noreply.github.com>
2023-04-19 09:39:44 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,10 @@
+def addition(num1, num2):
+    print('Addition is: ', num1 + num2)
+
+
+def subtraction(num1, num2):
+    print('Subtraction is: ', num1 - num2)
+
+
+if __name__ == "__main__":
+    print('Welcome')
